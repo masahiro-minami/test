235f1d237bd0295f1f5fe11e5641b53a7a50f89a
Masahiro Minami <masahiro.minami@careritz.co.jp>
2014-05-29 14:02:18 +0900

    change java code2

--- test.java
+++ test.java
@@ -2,15 +2,16 @@ public class test {
   public test() {
   }
 
-  public void a() {
+  public void _a() {
     //aaaa
   }
 
   public void b() {
     //bbb
+    int a = 0;
   }
 
   public void c() {
-    //bbb
+    //ccc
   }
 }
